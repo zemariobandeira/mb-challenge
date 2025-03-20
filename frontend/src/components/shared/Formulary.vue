6b06e447016795628e13771620659c49dff5737b
Mario Bandeira <josemariobandeira@hotmail.com>
2025-03-20 12:13:47 -0300

    feat(component): add formulary shared component

---
+++ frontend/src/components/shared/Formulary.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { provide, ref, toRaw } from "vue";
+
+  const emit = defineEmits(["submitData"]);
+
+  const formData = ref({});
+
+  function setFormData(name, value) {
+    formData.value[name] = value;
+  }
+
+  function onSubmitData() {
+    emit("submitData", toRaw(formData.value));
+  }
+
+  provide("formData", formData);
+  provide("setFormData", setFormData);
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="onSubmitData">
+      <slot></slot>
+    </form>
+  </div>
+</template>
