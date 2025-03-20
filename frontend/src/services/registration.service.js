e738c494eccf07ee770d7bc61b54a8e8c73e076c
Mario Bandeira <josemariobandeira@hotmail.com>
2025-03-20 17:41:27 -0300

    feat: add registration service abstraction

---
+++ frontend/src/services/registration.service.js
@@ -0,0 +1,43 @@
+const service = {
+  apiURL: '',
+  get: async function (endpoint) {
+    try {
+      const response = await fetch(this.apiURL + endpoint, { method: 'GET' });
+
+      if (!response.ok)
+        throw new Error('Houve um erro na requisição');
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return error.message
+        ? error.message
+        : 'Erro desconhecido'
+    }
+  },
+  post: async function (endpoint, body) {
+    try {
+      const response = await fetch(this.apiURL + endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok)
+        throw new Error('Houve um erro na requisição');
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return error.message
+        ? error.message
+        : 'Erro desconhecido'
+    }
+  }
+}
+
+export default service
