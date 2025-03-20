3f5a4fb75ad3c1d4ca502ef8172a85aca5a0ac30
Mario Bandeira <josemariobandeira@hotmail.com>
2025-03-20 12:03:49 -0300

    refactor: change folder and files structure to fit my personal preferences

---
+++ frontend/src/components/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import RegisterForm from '@/components/shared/RegisterForm.vue';
+</script>
+
+<template>
+  <div>
+    <RegisterForm />
+  </div>
+</template>
+
+<style>
+</style>
---
+++ frontend/src/components/shared/RegisterForm.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    REGISTER FORM COMPONENT
+  </div>
+</template>
