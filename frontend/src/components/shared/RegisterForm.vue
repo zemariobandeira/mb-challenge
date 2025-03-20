ff08ccb76a8f17b241cee3c6171d24595dba9de4
Mario Bandeira <josemariobandeira@hotmail.com>
2025-03-20 12:29:42 -0300

    feat(component): create a test form component

--- frontend/src/components/shared/RegisterForm.vue
+++ frontend/src/components/shared/RegisterForm.vue
@@ -1,5 +1,30 @@
+<script setup>
+import Formulary from '@/components/shared/Formulary.vue';
+import FormField from '@/components/shared/FormField.vue';
+</script>
+
 <template>
   <div>
-    REGISTER FORM COMPONENT
+    <Formulary @submit-data="console.log($event)">
+      <FormField
+        id="test1"
+        type="text"
+        name="input1"
+        text="Input 1"
+      />
+      <FormField
+        id="test2"
+        type="text"
+        name="input2"
+        text="Input 2"
+      />
+      <FormField
+        id="test3"
+        type="text"
+        name="input3"
+        text="Input 3"
+      />
+      <button type="submit">Send</button>
+    </Formulary>
   </div>
 </template>
