f9afe6702cb69c623ad2a0e80056fadb554c8d27
Mario Bandeira <josemariobandeira@hotmail.com>
2025-03-20 22:56:28 -0300

    build: add npm serve command to run the app in production mode

--- backend/server.js
+++ backend/server.js
@@ -13,28 +13,28 @@ app.use(express.static(path.resolve('dist')))
 
 app.post('/registration', async (req, res) => {
   const data = req.body
-
+  
   const accountRegister = data && data.cpf
-    ? new PersonalAccount()
-    : new CompanyAccount()
-
+  ? new PersonalAccount()
+  : new CompanyAccount()
+  
   const isValid = accountRegister.validate(data)
-
+  
   if (!isValid)
     return res
-      .status(400)
-      .json({ message: 'Existem campos não preenchidos. Tente novamente.' })
-
+  .status(400)
+  .json({ message: 'Existem campos não preenchidos. Tente novamente.' })
+  
   const isRegistered = await accountRegister.register(data);
-
+  
   if (!isRegistered)
     return res
-      .status(422)
-      .json({ message: 'Houve um erro durante o cadastro' })
-
+  .status(422)
+  .json({ message: 'Houve um erro durante o cadastro' })
+  
   return res
-    .status(201)
-    .json({ message: 'Conta criada com sucesso' })
+  .status(201)
+  .json({ message: 'Conta criada com sucesso' })
 })
 
 app.use('*', (req, res) => {
@@ -42,5 +42,5 @@ app.use('*', (req, res) => {
 })
 
 app.listen(3000, () => {
-  console.log(`Listening at http://localhost:${3000}`)
+  console.log(`Listening at http://localhost:${3000}/registration`)
 })
