a470006a1777b0a9ce7e39f60f33e8441ed30674
Mario Bandeira <josemariobandeira@hotmail.com>
2025-03-20 23:22:28 -0300

    refactor(composable): add type guard in useSteps composable

--- frontend/src/composables/useSteps.js
+++ frontend/src/composables/useSteps.js
@@ -1,8 +1,24 @@
 import { computed, ref } from "vue";
 
-export default function useSteps(total) {
-  const _current = ref(1)
-  const _total = ref(total)
+function parseTotalParam(total) {
+  if (isNaN(total)) throw 'Total of steps must be a number';
+  if (total <= 0) throw "Total of steps must be bigger than zero";
+
+  return total;
+}
+
+function parseInitialParam(initialStep, totalSteps) {
+  if (initialStep === undefined) return false
+  if (isNaN(initialStep)) throw 'Initial step must be a number';
+  if (initialStep > totalSteps) throw "Initial step must be smaller or equals than total steps";
+  if (initialStep <= 0) throw "Initial step must be bigger than zero";
+
+  return steps;
+}
+
+export default function useSteps(total, initial) {
+  const _total = ref(parseTotalParam(total))
+  const _current = ref(parseInitialParam(initial, _total.value) || 1);
 
   const currentStep = computed(() => _current.value)
   const totalSteps = computed(() => _total.value)
